bae1afcd2c5a81105cf6bf06072d199cdd4ec0f6
Derek Kong <derekklc@MacBook-Pro-3.local>
2023-04-21 12:27:46 +1000

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'some-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './app.custom.css'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  abcde = '12345';
 }
