23d74fe018109f28ae57ce7fb73d07a9bf79b19c
Derek Kong <derekklc@MacBook-Pro-3.local>
2023-04-25 22:44:21 +1000

    feature component of hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mockheros';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
+  // onSelection = (heroId: number) => {
+  //   this.selectedHero = this.heroes.find((person) => person.id === heroId);
+  // };
+  onSelection(heroId: number): void {
+    this.selectedHero = this.heroes.find((person) => person.id === heroId);
+  }
 }
---
+++ src/app/heroes/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+  color: gold;
+  font-size: 50px;
+`;
+
+export const StyledSpan = styled.span`
+  color: #393a4b;
+  border: 3px solid red;
+`;
